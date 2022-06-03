4632cd4b86949b1eb838039718a230f820daf576
avpconnect <104966550+avpconnect@users.noreply.github.com>
2022-06-04 03:27:13 +0700

    Update combo.php

--- combo.php
+++ combo.php
@@ -1,1 +1,22 @@
-8
+{"Version":"1.08",
+
+"Url":"https://github.com/avpconnect/app/raw/main/combo.zip" ,
+
+"Changelog":"  
+
+อ้ฟเดทไฟล์ล่าสุด
+",
+
+"latestVersion": "1.0.0",
+
+"latestVersionCode": "1",
+
+"url": "",
+
+ "releaseNotes": [
+
+ "-Demo"
+
+  ]
+
+}
